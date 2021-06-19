5cea252822e872b75420fcb69ee497a81d2b0da3
matheusdevelope <83522408+matheusdevelope@users.noreply.github.com>
2021-06-18 20:01:31 -0400

    add menu

---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Menu.css'
+
+export default ()=>{
+    return(
+        <div className='MenuArea'>
+            <div className='MenuItem'>
+                Agendamentos
+            </div>
+            <div className='MenuItem'>
+                Financeiro
+            </div>
+            <div className='MenuItem'>
+                Serviços
+            </div>
+        </div>
+    )
+}
