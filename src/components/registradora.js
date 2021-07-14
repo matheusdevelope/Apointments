5558a3fe0bbda50617b4ba27648fd9c56757f73a
Matheus-Se7e <emersonmatheustu@gmail.com>
2021-07-14 18:19:31 -0400

    add

--- src/components/registradora.js
+++ src/components/registradora.js
@@ -1,8 +1,12 @@
-import React from'react'
+import React, { useState } from'react'
 import './registradora.css'
 import CloseRounded from '@material-ui/icons/CloseRounded';
 
-export default ()=>{
+export default (data)=>{
+    const [formPagamento,setFormPagamento] = useState("DINHEIRO")
+    const [descont, setDescont]= useState(00,00)
+    const [valueFinal, setValueFinal]= useState("")
+    const [obs, setObs]= useState("")
 const dados = [{
     id:1,
     name:"PERFURAÇÃO SEPTO",
@@ -19,6 +23,18 @@ const dados = [{
     quantidade: 1  
 },
 ]
+const datasend = {
+    client_id:data.client_id,
+    client_name:data.FK_client_id.name,
+    service_id:data.id,
+    value_total:data.value,
+    descont: descont,
+    value_final: valueFinal,
+    observation: obs,
+    status: F,
+    formPagamento: formPagamento,
+    user_inclusion:"01",
+    }
 const handleCloseRegister =()=>{}
 
 return(
@@ -26,16 +42,18 @@ return(
 <div className='bodyRegister'>
     <div className='headerRegister'>
         <button />
-            <a>REGISTRADORA</a>
+            <a className='Title'>REGISTRADORA</a>
         <button onClick={handleCloseRegister}>
             <CloseRounded/>
         </button>
     </div>
     <div className='HeaderRegister2'>
     <div className='InfoClientRegister'>
-        <div className='NameClientRegister'>Matheus Teixeira</div>
+        <div className='NameClientRegister'>2023- Matheus Teixeira</div>
         <div className='ObsRegister'>Esse serviço foi acertado pelo Machadin,
-         ele pediu pra fazer mais barato.</div>
+         ele pediu pra fazer mais barato. Esse serviço foi acertado pelo Machadin,
+         ele pediu pra fazer mais barato.
+         </div>
     </div>
     <div className='PaiScroll'>
     <div className='ListItensServiceRegister'>
@@ -52,10 +70,19 @@ return(
     </div>
     </div>
     <div className='PagamentoRegister'>
-        <div>Sub Total<input className='ValueTotal' placeholder='500.00'/></div>
-        <div>Desconto<input className='ValueDescont' placeholder='200.00'/></div>
+        <div>Sub Total<input className='ValueTotal' placeholder={data.value}/></div>
+        <div>Desconto<input className='ValueDescont' placeholder={descont}/></div>
         <div>Total<input className='ValueFinal' placeholder='320.90'/></div>
-        <div>F. Pagamento<input className='PagamentForm' placeholder='Forma Pag.'/></div>
+        <div className="ListPagamento">
+            Pagamento
+        <ul>
+        <a className='TextFormSelected'>{formPagamento}</a>
+        <li><a href="#" onClick={()=>{setFormPagamento("DINHEIRO")}}>DINHEIRO</a></li>
+            <li onClick={()=>{setFormPagamento("CARTÃO")}}><a href="#">CARTÃO</a></li>
+            <li onClick={()=>{setFormPagamento("PIX")}}><a href="#" >PIX</a></li>
+            <li onClick={()=>{setFormPagamento("OUTROS")}}><a href="#" >OUTROS</a></li>
+        </ul>
+        </div>
         <button>OK</button>
     </div>
         </div>
